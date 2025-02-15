8543c23fdcea2c10621e2ed49b162f3f071b53c8
Frans <frans@fra3.nl>
2025-02-15 12:38:11 +0000

    add SP name

--- azure/scripts/create_sp.sh
+++ azure/scripts/create_sp.sh
@@ -1,1 +1,27 @@
-az ad sp create-for-rbac --name sp_tf_acc --role Contributor --scopes /subscriptions/d24a0722-0ec9-4434-aa91-2f83f4c5cd8a
+#!/bin/bash
+
+# Variables
+source ./test-environment.sh
+
+# Check if Azure CLI is installed
+if ! command -v az &> /dev/null; then
+  echo "Azure CLI not found. Please install it and try again."
+  exit 1
+fi
+
+# Check if already logged in to Azure
+if ! az account show &> /dev/null; then
+  echo "Not logged into Azure. Logging in..."
+  az login --service-principal \
+    --username "$SERVICE_PRINCIPAL_APP_ID" \
+    --password "$SERVICE_PRINCIPAL_PASSWORD" \
+    --tenant "$TENANT_ID"
+else
+  echo "Already logged into Azure."
+fi
+
+# Set Azure Subscription
+echo "Set subscription to: $SUBSCRIPTION_ID"
+az account set --subscription "$SUBSCRIPTION_ID"
+
+az ad sp create-for-rbac --name $SERVICE_PRINCIPAL_NAME --role Contributor --scopes /subscriptions/$SUBSCRIPTION_ID
