8f832c02ca601caece38be608cdcea309be6af7b
Frans Koster <56426142+FransKoster@users.noreply.github.com>
2025-10-29 16:38:16 +0100

    Delete azure directory

--- azure/modules/resource_group/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "resource_group_names" {
-  description = "The names of the created resource groups"
-  value       = [for rg in azurerm_resource_group.rgs : rg.name]
-}
-
-output "resource_group_locations" {
-  description = "The locations of the created resource groups"
-  value       = [for rg in azurerm_resource_group.rgs : rg.location]
-}
--- azure/test/resource_groups_locals.tf
+++
@@ -1,8 +0,0 @@
-# Define a list of resource groups
-locals {
-  resource_groups = [
-    { name = "rg-one", location = "swedencentral" },
-    { name = "rg-two", location = "westeurope" },
-    { name = "rg-three", location = "northeurope" }
-  ]
-}
